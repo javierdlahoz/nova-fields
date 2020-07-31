b11bd3362ad7274380f5559ea8069de84a525367
Javier De la Hoz <javierdlahoz@gmail.com>
2020-07-31 16:47:15 +0200

    toggle button for field

---
+++ resources/js/components/toggle_button/DetailField.vue
@@ -0,0 +1,20 @@
+<template>
+    <panel-item :field="field">
+        <template slot="value">
+            <div></div>
+        </template>
+    </panel-item>
+</template>
+
+<script>
+
+export default {
+    props: ["resource", "resourceName", "resourceId", "field"],
+
+    data: () => ({ theData: [] }),
+
+    created() {
+
+    }
+};
+</script>
